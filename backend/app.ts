eba0d47e71ce5732e2ee78fab229c18966c3745b
Sid Thakur <sidt@retool.com>
2022-12-18 21:29:26 -0800

    project setup

---
+++ backend/app.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import path from 'path'
+
+const app = express()
+const port = 3201
+
+app.use(express.static(path.resolve(__dirname, '../build/frontend')))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// Handle GET requests to /api route
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from server!' })
+})
+
+app.get('/api/greeting', (req, res) => {
+  const name = 'World'
+  res.setHeader('Content-Type', 'application/json')
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }))
+})
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../build/frontend', 'index.html'))
+})
+
+app.listen(port, () => {
+  console.log('Ayy lmao2')
+  return console.log(`Express is listening at http://localhost:${port}`)
+})
